a922df941420da8acf8b7178f51e6a16d988e1fa
Nathan Hoos <thwackyy.y@gmail.com>
2024-05-05 23:02:04 +0000

    fix ci

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 from setuptools_rust import Binding, RustExtension
-import toml
 
-# Load the contents of your pyproject.toml
-with open('pyproject.toml', 'r') as pyproject_file:
-    pyproject_data = toml.load(pyproject_file)
 
-# Extract package metadata
-metadata = pyproject_data.get('tool', {}).get('poetry', {})
+metadata = {
+    'name': "function-sampler",
+    'version': "0.1.5",
+    'description': "Function calling Logit Sampler",
+    'long_description': "", 
+    'authors': ["unaidedelf8777"],
+    'author_email': "thwackyy.y@gmail.com",
+    'license': "Apache 2.0",
+    'classifiers': [
+        "Programming Language :: Rust",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+    'url': "https://github.com/unaidedelf8777/function-sampler/",
+}
 
-# Define the Rust extension module, assuming it's the same name as the package name
 rust_extensions = [
     RustExtension(
-        f"function_sampler.fsm.fsm_utils",  # Python package.module for the Rust extension
-        f"./Cargo.toml",  # Path to Cargo.toml of Rust extension
+        f"function_sampler.fsm.fsm_utils",
+        f"./Cargo.toml",
         binding=Binding.PyO3,
         features=["default"],
         args=["--profile=release"]
@@ -21,22 +29,20 @@ rust_extensions = [
 ]
 
 setup(
-    name=metadata.get('name'),
-    version=metadata.get('version'),
-    author=",".join(metadata.get('authors', [])),
-    author_email=metadata.get('emails', [''])[0],
-    description=metadata.get('description', ''),
-    long_description=metadata.get('description', ''),
+    name=metadata['name'],
+    version=metadata['version'],
+    author=metadata['authors'][0].split(" <")[0],  # Extracting name
+    author_email=metadata['author_email'],
+    description=metadata['description'],
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url=metadata.get('homepage', ''),
-    classifiers=metadata.get('classifiers', []),
-    keywords=",".join(metadata.get('keywords', [])),
-    license=metadata.get('license', ''),
+    url=metadata['url'],
+    classifiers=metadata['classifiers'],
+    license=metadata['license'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.9',
-    setup_requires=['setuptools-rust', 'wheel', 'toml'],
+    setup_requires=['setuptools_rust', 'wheel'],
     rust_extensions=rust_extensions,
 )
-
