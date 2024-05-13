d9dcd1d28809fe28fad96082dd6a1cecc2f217a0
Nathan Hoos <thwackyy.y@gmail.com>
2024-05-13 13:16:45 +0000

    it better work now

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_dependencies():
 
 metadata = {
     'name': "function-sampler",
-    'version': "0.1.8",
+    'version': "0.1.9",
     'description': "Function calling Logit Sampler",
     'long_description': "", 
     'authors': ["unaidedelf8777"],
