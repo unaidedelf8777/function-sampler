cc4a28f9f6e9aa76631b295843905932dc526e2b
Nathan Hoos <thwackyy.y@gmail.com>
2024-05-13 16:46:34 +0000

    CI works. Revert tool call sampler to old.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read_dependencies():
 
 metadata = {
     'name': "function-sampler",
-    'version': "0.2",
+    'version': "0.2.1",
     'description': "Function calling Logit Sampler",
     'long_description': "", 
     'authors': ["unaidedelf8777"],
